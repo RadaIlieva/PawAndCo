0f807d8c105fcb54deaf029ca8b49e70bc98d29e
RadaIlieva <radaivajlovailieva@gmail.com>
2025-11-08 14:18:47 +0200

    fix sever.js

--- backend/server.js
+++ backend/server.js
@@ -17,18 +17,36 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ✅ позволяваме PATCH и Authorization хедъра
-app.use(cors({
-  origin: "http://127.0.0.1:5500",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+
 app.use("/api/products", product);
 app.use("/api/orders", orderRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/admin", adminRoutes);
 
+app.get("/", (req, res) => {
+  res.send("🐾 Paw&Co backend is running successfully!");
+});
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+);
+
+//  SERVER START 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
+);
