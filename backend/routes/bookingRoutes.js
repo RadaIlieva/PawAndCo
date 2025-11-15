c79d8552975bd0522fbc1d0f02978e25b00da7a5
RadaIlieva <radaivajlovailieva@gmail.com>
2025-11-15 17:16:30 +0200

    prices

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -10,27 +10,16 @@ import { verifyAdmin } from "../middleware/verifyAdmin.js";
 
 const router = express.Router();
 
-/* ------------------------- КЛИЕНТ ------------------------- */
-
-// Клиент – зарежда всички резервации (за да може да оцвети календара)
 router.get("/", getBookings);
 
-// Клиент – създава резервация
 router.post("/", createBooking);
 
-
-/* ------------------------- АДМИН -------------------------- */
-
-// Админ – създава резервация ръчно
 router.post("/admin", verifyAdmin, createBookingAdmin);
 
-// Админ – получава пълен списък с резервации
 router.get("/admin/all", verifyAdmin, getBookings);
 
-// Админ – редактира резервация
 router.patch("/:id", verifyAdmin, updateBooking);
 
-// Админ – изтрива резервация
 router.delete("/:id", verifyAdmin, deleteBooking);
 
 
